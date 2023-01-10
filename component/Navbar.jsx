a8bbbcc0e6335ff7b560d4ea7f2efa0e9e1d6efa
Akbar Rizky Rabbani <39977486+AkbarRizkyR@users.noreply.github.com>
2023-01-10 10:38:39 +0700

    Fixing New One

--- component/Navbar.jsx
+++ component/Navbar.jsx
@@ -1,10 +1,11 @@
-import Image from 'next-image'
+import Image from 'react-image'
 import React from 'react'
+
 const Navbar = () => {
   return (
     <div className='fixed w-full h-20 shadow-xl z-[100]'>
         <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
-            <Image src=".../public/assets/Logo.jpg" alt="/" width="125" height="50" />
+            <Image src="../public/assets/Logo.jpg" alt="/" width="125" height="50" />
         </div>
     </div>
   )
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,7 @@
+import React from 'react';
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import Navbar from '../component/Navbar'
-
-const inter = Inter({ subsets: ['latin'] })
+import Image from 'react-image'
+import Navbar from '../component/Navbar.jsx'
 
 export default function Home() {
   return (
